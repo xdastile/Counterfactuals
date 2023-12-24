3fbaba039512fa7f8d402b5c624375fb8410cc0d
xdastile <75782006+xdastile@users.noreply.github.com>
2023-12-24 07:49:38 +0200

    Update pso_dastile_counterfactuals.m

--- pso_dastile_counterfactuals.m
+++ pso_dastile_counterfactuals.m
@@ -4,39 +4,6 @@ close all;
 
 data = readtable("german_dataset.csv");
 
-columns={'Target','Age_years_', 'ConcurrentCredits', 'CreditAmount','DurationOfCredit_month_', 'ForeignWorker','LengthOfCurrentEmployment', 'MostValuableAvailableAsset','PaymentStatusOfPreviousCredit', 'Purpose', 'Sex_MaritalStatus','TypeOfApartment', 'ValueSavings_Stocks'};
-
-
-cols=columns(ismember(columns,data.Properties.VariableNames));
-data=data(:,cols);
-
-[n,m]=size(data);
-rows=(1:n);
-
-
-x=data(:,2:m);
-y=data(:,1);
-y=table2cell(y);
-
-%xtrain=rows2vars(xtrain);
-%train_data=table2cell(train_data);
-y =categorical(y);
-y = onehotencode(y, 2);
-x=table2array(x);
-x=x';
-y=y';
-
-trainFcn = 'trainscg';
-hiddenLayerSize=[20,10,6];
-net=patternnet(hiddenLayerSize, trainFcn);
-net.divideParam.trainRatio=70/100;
-net.divideParam.valRatio=15/100;
-net.divideParam.testRatio=15/100;
-
-[net,tr]= train(net,x,y );
-
-ypred=net(x);
-
 
 
 
@@ -117,8 +84,8 @@ rng default % For reproducibility
 fobj = @(z) Dastile_Optimizer(z,Y, mad_values, model);
 
 % Define the PSO's paramters 
-noP = 20;
-maxIter = 70;
+noP = 40;
+maxIter = 50;
 wMax = 0.9;
 wMin = 0.2;
 c1 = 2;
